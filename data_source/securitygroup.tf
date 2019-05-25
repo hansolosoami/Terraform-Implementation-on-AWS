5c7a47e3584ee3387bf8ff9d188cf711ee50eac9
pushpender singh <pushpenders196@gmail.com>
2019-05-25 17:56:38 +0530

    version 4

--- data_source/securitygroup.tf
+++ data_source/securitygroup.tf
@@ -1,20 +1,20 @@
-data "aws_ip_ranges" "european_ec2" {
-  regions = [ "eu-west-1", "eu-central-1" ]
+data "aws_ip_ranges" "asia_ec2" {
+  regions = [ "ap-south-1", "ap-southeast-2" ]
   services = [ "ec2" ]
 }
 
-resource "aws_security_group" "from_europe" {
- name = "from_europe"
+resource "aws_security_group" "from_asia" {
+ name = "from_asia"
 
   ingress {
     from_port = "443"
     to_port = "443"
     protocol = "tcp"
-    cidr_blocks = [ "${data.aws_ip_ranges.european_ec2.cidr_blocks}" ]
+    cidr_blocks =  "${data.aws_ip_ranges.asia_ec2.cidr_blocks}"
   }
-  tags {
-    CreateDate = "${data.aws_ip_ranges.european_ec2.create_date}"
-    SyncToken = "${data.aws_ip_ranges.european_ec2.sync_token}"
+  tags = {
+    CreateDate = "${data.aws_ip_ranges.asia_ec2.create_date}"
+    SyncToken = "${data.aws_ip_ranges.asia_ec2.sync_token}"
   }
 
 }
--- data_source/vars.tf
+++ data_source/vars.tf
@@ -7,6 +7,5 @@ variable "AMIS" {
     ap-south-1 = "ami-0dd423361176c4c3e"
     us-west-2 = "ami-06b94666"
     ap-southeast-2 = "ami-09d9316fe73403e7b"
-  }
+  }                                                                           
 }
-
